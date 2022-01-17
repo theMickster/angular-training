7445587a45b5501face686d9eab3f38e4cb8a45e
Mick Letofsky <mletofsky@outlook.com>
2022-01-17 06:51:22 -0700

    TOH Tests

--- ps-angular-toh-unit-testing/src/app/heroes/heroes.component.spec.ts
+++ ps-angular-toh-unit-testing/src/app/heroes/heroes.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, Input } from "@angular/core";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { of } from "rxjs";
@@ -6,90 +7,98 @@ import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 
 describe('HeroesComponent', () => {
-    let sut: HeroesComponent;
-    let heroesList: Hero[];
-    let mockHeroService;
+  let sut: HeroesComponent;
+  let heroesList: Hero[];
+  let mockHeroService;
 
-    beforeEach(() => {
-        heroesList = [
-            { id: 1, name:'Babe Ruth', strength: 18 },
-            { id: 2, name:'Honus Wagner', strength: 15 },
-            { id: 3, name:'Cy Young', strength: 17 },
-            { id: 4, name:'Rogers Hornsby', strength: 19 },
-            { id: 5, name:'Ty Cobb', strength: 16 },
-            { id: 6, name:'Walter Johnson', strength: 18 },
-            { id: 7, name:'Lou Gerhig', strength: 15 }
-        ]
+  beforeEach(() => {
+    heroesList = [
+      { id: 1, name: 'Babe Ruth', strength: 18 },
+      { id: 2, name: 'Honus Wagner', strength: 15 },
+      { id: 3, name: 'Cy Young', strength: 17 },
+      { id: 4, name: 'Rogers Hornsby', strength: 19 },
+      { id: 5, name: 'Ty Cobb', strength: 16 },
+      { id: 6, name: 'Walter Johnson', strength: 18 },
+      { id: 7, name: 'Lou Gerhig', strength: 15 }
+    ]
 
-        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
-        sut = new HeroesComponent(mockHeroService);
+    sut = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete hero', () => {
+    it('should delete the second hero from hero list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      sut.heroes = heroesList;
+
+      expect(sut.heroes.length).toBe(7);
+
+      sut.delete(heroesList[1]);
+
+      expect(sut.heroes.length).toBe(6);
+
+      expect(sut.heroes).toEqual(
+        [heroesList[0], heroesList[2], heroesList[3], heroesList[4], heroesList[5], heroesList[6]]
+      )
+    })
+
+    it('should correctly call the delete method of the hero service', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      sut.heroes = heroesList;
+      expect(sut.heroes.length).toBe(7);
+
+      sut.delete(heroesList[3]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(heroesList[3]);
     });
 
-    describe('delete hero', () =>{
-        it('should delete the second hero from hero list', () => {
-            mockHeroService.deleteHero.and.returnValue(of(true));
-            sut.heroes = heroesList;
-
-            expect(sut.heroes.length).toBe(7);
-            
-            sut.delete(heroesList[1]);
-
-            expect(sut.heroes.length).toBe(6);
-
-             expect(sut.heroes).toEqual(
-                 [ heroesList[0], heroesList[2], heroesList[3], heroesList[4], heroesList[5], heroesList[6] ]
-             )
-        })
-
-        it('should correctly call the delete method of the hero service', () =>{
-            mockHeroService.deleteHero.and.returnValue(of(true));
-            sut.heroes = heroesList;
-            expect(sut.heroes.length).toBe(7);
-            
-            sut.delete(heroesList[3]);            
-
-            expect(mockHeroService.deleteHero).toHaveBeenCalledWith(heroesList[3]);
-        });
-
-    });
+  });
 
 });
 
-describe('Heroes Component Integration Tests', () =>{
-    let sut: ComponentFixture<HeroesComponent>;
-    let heroesList: Hero[];
-    let mockHeroService;
+describe('Heroes Component Integration Tests', () => {
+  let sut: ComponentFixture<HeroesComponent>;
+  let heroesList: Hero[];
+  let mockHeroService;
 
-    beforeEach(() =>{
-        heroesList = [
-            { id: 1, name:'Babe Ruth', strength: 18 },
-            { id: 2, name:'Honus Wagner', strength: 15 },
-            { id: 3, name:'Cy Young', strength: 17 },
-            { id: 4, name:'Rogers Hornsby', strength: 19 },
-            { id: 5, name:'Ty Cobb', strength: 16 },
-            { id: 6, name:'Walter Johnson', strength: 18 },
-            { id: 7, name:'Lou Gerhig', strength: 15 }
-        ];
+  @Component({ selector: 'app-hero', template: '<div id="fakeHeroDiv"></div>' })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+  }
 
-        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHeroes', 'deleteHero']);
+  beforeEach(() => {
+    heroesList = [
+      { id: 1, name: 'Babe Ruth', strength: 18 },
+      { id: 2, name: 'Honus Wagner', strength: 15 },
+      { id: 3, name: 'Cy Young', strength: 17 },
+      { id: 4, name: 'Rogers Hornsby', strength: 19 },
+      { id: 5, name: 'Ty Cobb', strength: 16 },
+      { id: 6, name: 'Walter Johnson', strength: 18 },
+      { id: 7, name: 'Lou Gerhig', strength: 15 }
+    ];
 
-        TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
-            providers: [
-                { provide: HeroService, useValue: mockHeroService }
-            ],
-            schemas: [NO_ERRORS_SCHEMA]
-        })
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHeroes', 'deleteHero']);
 
-        sut = TestBed.createComponent(HeroesComponent);
-    });
-
-    it('should set heroes correctly from service', () =>{
-        mockHeroService.getHeroes.and.returnValue(of(heroesList));
-        sut.detectChanges();
-
-        expect(sut.componentInstance.heroes.length).toBe(7);
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        FakeHeroComponent
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+      schemas: []
     })
+
+    sut = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(heroesList));
+    sut.detectChanges();
+
+    expect(sut.componentInstance.heroes.length).toBe(7);
+  })
 
 });
