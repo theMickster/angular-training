8e2488aacc77e515472ca3e885a2cf5f2b78f7ee
Mick Letofsky <mick.letofsky@ey.com>
2022-07-23 15:50:47 -0600

    First check-in from the course.

---
+++ reactive-angular-workshop/src/app/components/hero-table/hero-table.component.ts
@@ -0,0 +1,37 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DEFAULT_PAGE, Hero, HeroService } from '../../services/hero.service';
+
+@Component({
+    selector: 'rx-hero-table',
+    templateUrl: './hero-table.component.html',
+    styleUrls: ['./hero-table.component.scss'],
+})
+export class HeroTableComponent {
+    heroes$ : Observable<Hero[]> = this.hero.heroes$;
+    search$ = this.hero.searchBS;
+    page$ = this.hero.userPage$;
+    limit$ = this.hero.limitBS;
+    totalResults$ = this.hero.totalResults$;
+    totalPages$ = this.hero.totalPages$;
+
+    constructor(public hero: HeroService) {
+    }
+
+    doSearch(event: any)
+    {
+        this.hero.searchBS.next(event.target.value);
+        this.hero.pageBS.next(DEFAULT_PAGE);
+    }
+
+    movePageBy(moveBy : number) {
+        const currentPage = this.hero.pageBS.getValue();
+        this.hero.pageBS.next( currentPage + moveBy);
+    }
+
+    setLimit(limit: number){
+        this.hero.limitBS.next(limit);
+        this.hero.pageBS.next(DEFAULT_PAGE);
+    }
+}
---
+++ reactive-angular-workshop/src/app/services/hero.service.ts
@@ -0,0 +1,97 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { debounceTime, map, shareReplay, switchMap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+export interface Hero {
+    id: number;
+    name: string;
+    description: string;
+    thumbnail: HeroThumbnail;
+    resourceURI: string;
+    comics: HeroSubItems;
+    events: HeroSubItems;
+    series: HeroSubItems;
+    stories: HeroSubItems;
+}
+
+export interface HeroThumbnail {
+    path: string;
+    extendion: string;
+}
+
+export interface HeroSubItems {
+    available: number;
+    returned: number;
+    collectionURI: string;
+    items: HeroSubItem[];
+}
+
+export interface HeroSubItem {
+    resourceURI: string;
+    name: string;
+}
+
+// The URL to the Marvel API
+const HERO_API = `${environment.MARVEL_API.URL}/v1/public/characters`;
+
+// Our Limits for Search
+const LIMIT_LOW = 10;
+const LIMIT_MID = 25;
+const LIMIT_HIGH = 100;
+const LIMITS = [LIMIT_LOW, LIMIT_MID, LIMIT_HIGH];
+export const DEFAULT_SEARCH = '';
+export const DEFAULT_LIMIT = LIMIT_HIGH;
+export const DEFAULT_PAGE = 0;
+
+@Injectable({
+    providedIn: 'root',
+})
+export class HeroService {
+    limits = LIMITS;
+
+    searchBS = new BehaviorSubject<string>(DEFAULT_SEARCH);
+    limitBS = new BehaviorSubject<number>(DEFAULT_LIMIT);
+    pageBS = new BehaviorSubject<number>(DEFAULT_PAGE);
+
+    userPage$ = this.pageBS.pipe( map( page => page + 1) );
+
+    params$ = combineLatest([this.searchBS, this.limitBS, this.pageBS]).pipe( 
+        map( ([searchTerm, limit , page]) => {            
+            const params : any = {
+                apikey: environment.MARVEL_API.PUBLIC_KEY,
+                limit: `${limit}`,
+                offset: `${page * limit}`
+            };
+            if (searchTerm.length){
+                params.nameStartsWith = searchTerm;
+            }
+            return params;
+        })          
+    );
+
+    private heroesRespose$ = this.params$.pipe(
+        debounceTime(500),
+        switchMap(_params => 
+            this.http.get(HERO_API, {
+                params: _params
+            }
+        )),
+        shareReplay(1)
+    );
+
+    totalResults$ = this.heroesRespose$.pipe(
+        map((result:any) => result.data.total)
+    );
+
+    heroes$: Observable<Hero[]> = this.heroesRespose$.pipe(
+        map( (result : any) => result.data.results )
+    );
+
+    totalPages$ = combineLatest([this.totalResults$, this.limitBS]).pipe(
+        map(( [totalResults, limit]) => Math.ceil(totalResults / limit))
+    );
+    
+    constructor(private http: HttpClient) {}
+}
