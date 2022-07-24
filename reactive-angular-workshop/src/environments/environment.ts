897f789a67719fa54826913cb5b7f1904036bfb3
Mick Letofsky <mick.letofsky@ey.com>
2022-07-24 12:46:58 -0600

    Added env files

---
+++ reactive-angular-workshop/src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+    production: false,
+    MARVEL_API: {
+        URL: 'https://gateway.marvel.com:443',
+        PUBLIC_KEY: 'dc72ac3969367ab756894d8aa6372803',
+        PRIVATE_KEY: '843cbe172ec93be2b80f580de97353819a54ad5a',
+    },
+};
+
+if (environment.MARVEL_API.PUBLIC_KEY === 'INSERT YOUR KEY FIRST') {
+    /**
+     * To get access to the marvel API, you need to go to their site and sign up for an account.
+     * Go Here: https://developer.marvel.com/
+     *
+     * Once you have done that, in their portal, you will need to add http://localhost to their
+     * whitelisted domains. If you don't do this, it will fail for you.
+     */
+    document.body.innerHTML =
+        'INSERT YOUR KEY FIRST<BR>See <code>environments.ts</code> for instructions';
+    throw new Error('You must setup a public and private API key first.');
+}
