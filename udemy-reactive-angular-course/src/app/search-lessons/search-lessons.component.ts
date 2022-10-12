f90c8ec8fd944209d2e222c7d56e0fd803162391
Mick Letofsky <mick.letofsky@ey.com>
2022-10-12 07:01:11 -0600

    Completing section #5.

--- udemy-reactive-angular-course/src/app/search-lessons/search-lessons.component.ts
+++ udemy-reactive-angular-course/src/app/search-lessons/search-lessons.component.ts
@@ -1,20 +1,9 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay
-} from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
@@ -24,7 +13,11 @@ import {Lesson} from '../model/lesson';
 })
 export class SearchLessonsComponent implements OnInit {
 
-  constructor() {
+  searchResults$: Observable<Lesson[]>;
+
+  activeLesson: Lesson;
+
+  constructor(private coursesService: CoursesService) {
 
 
   }
@@ -34,6 +27,17 @@ export class SearchLessonsComponent implements OnInit {
 
   }
 
+  onSearch(search:string){
+    this.searchResults$ = this.coursesService.searchLessons(search);
+  }
+
+  openLesson(lesson: Lesson){
+    this.activeLesson = lesson;
+  }
+
+  onBackToSearch(){
+    this.activeLesson = null;
+  }
 }
 
 
