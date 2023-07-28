b59c690cb0ad5b8c7661bb12a40170d226380e6b
Mick Letofsky <mickl@knowledgeservices.com>
2023-07-28 10:39:28 -0600

    Updates galore

--- ps-angular-toh-unit-testing/README.md
+++
@@ -1,5 +0,0 @@
-# Pluralsight's Unit Testing in Angular Course
-
-Created by Joe Eames for Pluralsight.
-
-
--- ps-angular-toh-unit-testing/src/app/hero/hero.component.ts
+++ ps-angular-toh-unit-testing/src/app/hero/hero.component.ts
@@ -4,7 +4,7 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls:  ['./hero.component.css']
+  styleUrls:  ['./hero.component.scss']
 })
 export class HeroComponent {
   @Input() hero: Hero;
